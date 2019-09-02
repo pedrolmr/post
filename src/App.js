a124fbdeb759525258dd62ea332d9e47278d9cef
pedrolmr <pedroluismontesinosrivera@gmail.com>
2019-09-02 00:19:18 -0400

    created components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
-const App = (props) => {
-  return (
-    <div className="App">
-    
-    </div> 
-  );
+import { NavLink, Route, Switch } from 'react-router-dom'; 
+
+class App extends Component {
+  state = {
+    todos: []
+  }
+  render(){
+    return (
+      <div className="App">
+        <div className="Nav">
+          <NavLink to="/">Home</NavLink>
+          <NavLink to="/create">Create</NavLink>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
